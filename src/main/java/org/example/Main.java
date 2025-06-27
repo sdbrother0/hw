2249d25066066b527fafa4b9100632a281917591
sd <demin_sv@mail.ru>
2025-06-27 13:32:29 +0300

    tests add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,57 @@ import com.google.common.base.Stopwatch;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int my = 0; my < 100; my++) {
-            for (int test = 0; test < 2; test++) {
+        //Bulk Addition Test: Add 1,000,000 elements and measure:
+        for (int my = 0; my < 5; my++) {
+            for (int test = 0; test < 3; test++) {
                 Stopwatch stopwatch = Stopwatch.createStarted();
                 List list;
                 if (test == 1) {
                     list = new ArrayList();
+                } else if (test == 2){
+                    list = new LinkedList<>();
                 } else {
                     list = new CustomList();
                 }
-                for (int i = 0; i < 10_000_000; i++) {
+                for (int i = 0; i < 1_000_000; i++) {
                     list.add(i);
                 }
                 stopwatch.stop();
                 Duration duration = stopwatch.elapsed();
-                System.out.println(String.format("%s Duration Millis %s: %s", my, list.getClass(), duration.toMillis()));
+                System.out.println(String.format("%s Duration Millis add %s: %s", my, list.getClass(), duration.toMillis()));
+                Thread.sleep(1000);
+            }
+            Thread.sleep(1000);
+            System.out.println("---------------------------------------------------");
+        }
+
+        //
+        for (int my = 0; my < 5; my++) {
+            for (int test = 0; test < 3; test++) {
+
+                List list;
+                if (test == 1) {
+                    list = new ArrayList();
+                } else if (test == 2){
+                    list = new LinkedList<>();
+                } else {
+                    list = new CustomList();
+                }
+                for (int i = 0; i < 10_000; i++) {
+                    list.add(i);
+                }
+                Stopwatch stopwatch = Stopwatch.createStarted();
+                for (int i = 0; i < 10_000; i++) {
+                    list.remove(0);
+                }
+                stopwatch.stop();
+                Duration duration = stopwatch.elapsed();
+                System.out.println(String.format("%s Duration Millis remove 0 element %s: %s", my, list.getClass(), duration.toMillis()));
                 Thread.sleep(1000);
             }
             Thread.sleep(1000);
