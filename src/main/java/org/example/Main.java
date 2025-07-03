4033668c2cd33574934ac96c16d2a9a83afbd647
sd <demin_sv@mail.ru>
2025-07-03 16:33:09 +0300

    fib

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.hw02.FibonacciAlgorithms;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.printf("fibonacciRecursive: %d%n", FibonacciAlgorithms.fibonacciRecursive(10));
+        System.out.printf("fibonacciIterative: %d%n", FibonacciAlgorithms.fibonacciIterative(10));
     }
 }
