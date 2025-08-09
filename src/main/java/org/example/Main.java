d89411daa9b23c1964c97655a8c9429ddec61f42
sd <demin_sv@mail.ru>
2025-08-09 21:30:25 +0300

    hw04 donw

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,9 @@
 package org.example;
 
-import org.example.hw03.PerfTest;
+import org.example.hw04.PerfTest4;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        /*
-        HW03:
-        + Implement your own LinkedList + Implement your own queue & stack
-        + add your new linkedlist to the performance test from the hw01.
-        + compare performance of your different custom List implementations
-        + Implement your deque implementation based on your own linkedlist
-        + Implement your own dequeue implementation based on dynamic array
-         */
-        PerfTest.init();
+        PerfTest4.init();
     }
 }
--- src/test/java/org/example/hw01/CustomListTest.java
+++ src/test/java/org/example/hw01/CustomListTest.java
@@ -1,6 +1,5 @@
-package org.example;
+package org.example.hw01;
 
-import org.example.hw01.CustomList;
 import org.example.hw03.CustomLinkedList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
